c04f04e6dd5a4aa76b993764a45ca501031927cf
Ma-n-a-v <manupatel3345@gmail.com>
2024-10-22 16:03:03 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Feedbacks, Tech, Hero, Navbar, Works, StarsCavas } from './components';
+//import { div } from 'framer-motion/client';
+
 const App = () => {
   return (
-    <div>
-      3D Developer Portfolio
+  <BrowserRouter>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCavas />
+        </div>
     </div>
+  </BrowserRouter>
   )
 }
 
